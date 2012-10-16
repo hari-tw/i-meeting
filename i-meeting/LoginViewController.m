619c8394c7ee1093d6c2f766d7e50210849592fc
Harsha Ivaturi <harshai@thoughtworks.com>
2012-10-16 19:58:55 +0530

    Basic login button template

---
+++ i-meeting/LoginViewController.m
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.m
+//  i-meeting
+//
+//  Created by harshai on 16/10/12.
+//  Copyright (c) 2012 ThoughtWorks Technologies (India) Pvt. Ltd. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)onLoginClick:(id)sender {
+    NSLog(@"blag");
+}
+@end
