f60f33829c557516c4c34d5e87b67596d17e5057
Rashi Phuloria <rashiph@thoughtworks.com>
2012-12-06 17:28:57 +0530

    [Mansi/Rashi]#167 Created the username property in plist file.

--- i-meeting/MyMeetingViewController.m
+++ i-meeting/MyMeetingViewController.m
@@ -34,6 +34,28 @@
 {
     [self displayCalendar];
     [super viewDidLoad];
+    NSString *propertyListFilePath=[[NSBundle mainBundle] pathForResource:@"Property List" ofType:@"plist"];
+
+    NSDictionary *propertiesDictionary = [[NSDictionary alloc] initWithContentsOfFile:propertyListFilePath];
+    for (id key in propertiesDictionary)
+    {
+        if([(NSString *)key isEqualToString:@"UserName"]){
+            //id value = [propertiesDictionary objectForKey:key];
+            UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Hello!" message:@"Please enter your name:" delegate:self cancelButtonTitle:@"Continue" otherButtonTitles:nil];
+            alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+            UITextField * alertTextField = [alert textFieldAtIndex:0];
+            alertTextField.keyboardType = UIKeyboardTypeDefault;
+            alertTextField.placeholder = @"Enter your name";
+            [alert show];
+        }
+
+    }
+    
+    }
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    NSLog(@"Entered: %@",[[alertView textFieldAtIndex:0] text]);
+    
 }
 
 - (SignInHandler *)signInHandler
