3fe69b419969253e1688c17cb8009056966e8452
pragya <pragya.91.jain@gmail.com>
2012-12-07 11:17:03 +0530

    Akriti/Pragya Added the duration label for each meeting on view meeting room's schedule

--- i-meeting/CalendarViewController.m
+++ i-meeting/CalendarViewController.m
@@ -54,15 +54,29 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    NSString *durationString = @"";
     static NSString *cellIdentifier = @"Cell";
     CalendarCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
     id event = [self.events objectAtIndex:indexPath.row];
     NSDateComponents *eventStart = ((GTLCalendarEventDateTime *)[event valueForKey:@"start"]).dateTime.dateComponents;
     NSDateComponents *eventEnd = ((GTLCalendarEventDateTime *)[event valueForKey:@"end"]).dateTime.dateComponents;
-    int sampleHourToMin = ((eventStart.hour)*60)+eventStart.minute;
-    int sampleHourToMinEnd = (eventEnd.hour*60)+eventEnd.minute;
-    int sampleduration = sampleHourToMinEnd-sampleHourToMin;
-    NSLog(@"Hello   %d",sampleduration);
+    int startTimeInMinute = ((eventStart.hour)*60)+eventStart.minute;
+    int endTimeInMinute = (eventEnd.hour*60)+eventEnd.minute;
+    int duration = endTimeInMinute-startTimeInMinute;
+    if (duration < 60) {
+        durationString = [NSString stringWithFormat:@"%d minutes", duration];}
+    else {
+        int hour = duration/60;
+        int minutes = duration%60;
+        if (minutes == 0) {
+            durationString = [NSString stringWithFormat:@"%d hour", hour];
+        }
+        else {
+            durationString = [NSString stringWithFormat:@"%d hour %d minutes", hour,minutes];
+        }
+    }
+    
+    
 
     
     NSString *eventStartTime = [NSString stringWithFormat:@"%02d:%02d", eventStart.hour, eventStart.minute];
@@ -71,7 +85,7 @@
     
     cell.titleLabel.text = [event valueForKey:@"summary"];
     cell.timingsLabel.text = [NSString stringWithFormat:@"%@-%@", eventStartTime, eventEndTime];
-    
+    cell.durationLabel.text = [NSString stringWithFormat: @"Duration: %@", durationString];
     NSLog(@"%@", event);
     
     return cell;
