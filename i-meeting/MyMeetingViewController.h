cbad3b05ce26e75ff6e90366a190c4c803a1d502
Mansi Singhal <singhalmansi7@gmail.com>
2012-12-13 00:22:25 +0530

    [Mansi]#167 Now user can see his/her meetings.

--- i-meeting/MyMeetingViewController.h
+++ i-meeting/MyMeetingViewController.h
@@ -1,16 +1,10 @@
-//
-//  MyMeetingViewController.h
-//  i-meeting
-//
-//  Created by Manan on 11/6/12.
-//  Copyright (c) 2012 ThoughtWorks Technologies (India) Pvt. Ltd. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "CalendarViewController.h"
+#import "SignInHandler.h"
 
 @interface MyMeetingViewController : CalendarViewController
 
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) SignInHandler *signInHandler;
 
 @end
--- i-meeting/MyMeetingViewController.m
+++ i-meeting/MyMeetingViewController.m
@@ -1,76 +1,38 @@
-//
-//  MyMeetingViewController.m
-//  i-meeting
-//
-//  Created by Manan on 11/6/12.
-//  Copyright (c) 2012 ThoughtWorks Technologies (India) Pvt. Ltd. All rights reserved.
-//
-
 #import "MyMeetingViewController.h"
-#import "GTLCalendar.h"
-#import "DateTimeUtility.h"
-#import "CalendarCell.h"
-#import  "GTLCalendarEventDateTime.h"
-#import "RootViewController.h"
-#import "SignInHandler.h"
 
 @interface MyMeetingViewController ()
-
-@property (nonatomic, strong) GTLServiceCalendar *calendarService;
-@property (nonatomic, strong) NSArray *eventsSummaries;
-@property (nonatomic) BOOL isSignedIn;
-@property (nonatomic, strong) SignInHandler *signInHandler;
-
 @end
 
 @implementation MyMeetingViewController
 
-@synthesize eventsSummaries = _eventsSummaries;
-@synthesize calendarService = _calendarService;
-@synthesize isSignedIn = _isSignedIn;
-@synthesize signInHandler = _signInHandler;
-
-- (void)viewDidLoad
-{
-    [self displayCalendar];
-    [super viewDidLoad];
-   
-}
 
 - (void)awakeFromNib
 {
-    
     [self.signInHandler authorizeUser];
 }
-  
-- (SignInHandler *)signInHandler
-{
-    if (!_signInHandler) _signInHandler = [SignInHandler new];
-    return _signInHandler;
-}
 
-- (void)displayCalendar{
-    
-    NSString *userNameToViewCalendar = [self.signInHandler userEmail];
-    GTLQueryCalendar *query = [GTLQueryCalendar queryForEventsListWithCalendarId:userNameToViewCalendar];
-    query.timeMin =  [DateTimeUtility dateTimeForYear:2012 month:12 day:06 atHour:0 minute:0 second:0];
-    query.timeMax = [DateTimeUtility dateTimeForYear:2012 month:12 day:07 atHour:24 minute:0 second:0];
-    [self.signInHandler.calendarService executeQuery:query delegate:self didFinishSelector:@selector(didFinishQueryCalendar:finishedWithObject:error:)];
-}
-
-- (void)didFinishQueryCalendar:(GTLServiceTicket *)ticket finishedWithObject:(GTLObject *)object error:(NSError *)error
+- (void)viewDidLoad
 {
-    if (error) {
-        NSLog(@"%@", error);
-        return;
-    }
-    
-    GTLCalendarEvents *events = (GTLCalendarEvents *)object;
-    self.eventsSummaries = events.items;
     [super viewDidLoad];
+    [self.spinner startAnimating];
+    [self.spinner hidesWhenStopped];
+    [self performSegueWithIdentifier:@"myCalendarSeague" sender:self];
+    
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"myCalendarSeague"]) {
+        CalendarViewController *calendarViewController = segue.destinationViewController;
+        calendarViewController.viewTitle = @"My Meetings";
+        NSString* userEmail = [self.signInHandler userEmail];
+        calendarViewController.calendarId = userEmail;
+        [calendarViewController.signInHandler signInUser:@selector(displayCalendar:) withCalendarId:calendarViewController.calendarId withParentController:calendarViewController];
+    }
 }
 
 - (void)viewDidUnload {
     [super viewDidUnload];
 }
+
 @end
