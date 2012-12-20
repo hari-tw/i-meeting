2944446c6f51a3c89dde2d15b32a0367fb06449b
Sanchit Bahal <sanchit.bahal@live.com>
2012-12-20 13:20:13 +0530

    Removed redundant headers from files.

--- i-meeting/SignInViewController.h
+++ i-meeting/SignInViewController.h
@@ -1,11 +1,3 @@
-//
-//  SignInViewController.h
-//  i-meeting
-//
-//  Created by Sanchit Bahal on 19/12/12.
-//  Copyright (c) 2012 ThoughtWorks Technologies (India) Pvt. Ltd. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface SignInViewController : UIViewController
