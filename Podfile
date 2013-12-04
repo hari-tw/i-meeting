f8ddeabc02142bba17c93ad1a6d1c9bb8e479f89
Hari Balamani <linkwithhari@gmail.com>
2013-12-04 22:08:59 +0530

    Hari | Removing the ZBarSDK

--- Podfile
+++ Podfile
@@ -1,2 +1,1 @@
 pod 'Google-API-Client', '~> 0.0.1'
-pod 'ZBarSDK', '~> 1.3.1'
--- i-meeting/RootViewController.h
+++ i-meeting/RootViewController.h
@@ -1,11 +1,6 @@
 #import <UIKit/UIKit.h>
-#import "ZBarSDK.h"
 
 @interface RootViewController : UIViewController
-
-- (void)prepareQrCodeReader;
-- (NSString *)getScannedCode:(NSDictionary *)info;
 - (IBAction)signOut:(id)sender;
-
 @end
 
